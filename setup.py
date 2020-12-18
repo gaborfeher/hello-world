8a536df04fe495727aac06b5400c834145427f38
Gabor Feher <feherga@gmail.com>
2020-12-18 18:01:11 +0100

    setup & github workflow

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="hallo-world-gfeher",
+    version="0.0.1",
+    author="Gabor Feher",
+    author_email="feherga@gmail.com",
+    description="A small example package",
+    long_description='Long description',
+    long_description_content_type="text/plain",
+    url="https://github.com/gaborfeher/hello-world",
+    py_modules=['hello'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
