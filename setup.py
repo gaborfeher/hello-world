63811b66f8f921fa4903e379725603153a0c8aaf
Gabor Feher <feherga@gmail.com>
2020-12-18 18:18:36 +0100

    github workflow fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hallo-world-gfeher",
-    version="0.0.1",
+    version="0.0.7",
     author="Gabor Feher",
     author_email="feherga@gmail.com",
     description="A small example package",
